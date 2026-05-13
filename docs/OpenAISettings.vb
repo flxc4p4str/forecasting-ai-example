a7968f76f11f8ded527c6cc3973205f6367ea1cb
flxc4p4str <rdw@absolution.com>
2026-05-13 17:42:16 -0400

    required by AIController.vb

---
+++ docs/OpenAISettings.vb
@@ -0,0 +1,17 @@
+﻿Namespace intapi
+
+    Public Interface IOpenAISettings
+        Property OPENAI_MODEL As String
+        Property OPENAI_API_KEY As String
+    End Interface
+
+    Public Class OpenAISettings
+        Implements IOpenAISettings
+        Public Sub New()
+
+        End Sub
+        Public Property OPENAI_MODEL As String Implements IOpenAISettings.OPENAI_MODEL
+        Public Property OPENAI_API_KEY As String Implements IOpenAISettings.OPENAI_API_KEY
+    End Class
+
+End Namespace
